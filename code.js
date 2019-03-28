1a812e4f7edefe5825fa30957fa0bbed47c43a1f
PAwelZ97 <pawelzych97@gmail.com>
2019-03-28 16:26:43 +0100

    refactoring, code blocks moved, var to let

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
-var screen3D = new function () {
-    var screen = {
+let screen3D = new function () {
+    let screen = {
         x: 0,
         y: 0,
         width: 100,
         height: 100
     };
 
-    var canvs;
-    var contxt;
+    let canvs;
+    let contxt;
 
-    var key = {
+    let key = {
         front: false,
         back: false,
         left: false,
@@ -26,67 +26,22 @@ var screen3D = new function () {
         zoom_out: false
     };
 
-    var objectPool = [];
-
-    var multip = 100;
-
-    var initsize = 10;
-
-    var j = 0;
-
-    function Cube(x, y, z, x_size, y_size, z_size, multip) {
-        this.position = {
-            x: x,
-            y: y,
-            z: z
-        };
-        this.size = {
-            x: x_size * multip,
-            y: y_size * multip,
-            z: z_size * multip,
-        };
-        this.vertices = [new Point(x, y, z), new Point(x+this.size.x,y,z), new Point(x+this.size.x,y,z+this.size.z), new Point(x,y,z+this.size.z),
-            new Point(x, y+this.size.y, z), new Point(x+this.size.x,y+this.size.y,z), new Point(x+this.size.x,y+this.size.y,z+this.size.z), new Point(x,y+this.size.y,z+this.size.z)];
-
-        this.lines = [new Line(this.vertices[0],this.vertices[1]), new Line(this.vertices[1],this.vertices[2]),
-            new Line(this.vertices[2],this.vertices[3]),new Line(this.vertices[3],this.vertices[0]),
-
-            new Line(this.vertices[4],this.vertices[5]),new Line(this.vertices[5],this.vertices[6]),
-            new Line(this.vertices[6],this.vertices[7]),new Line(this.vertices[7],this.vertices[4]),
-
-            new Line(this.vertices[0],this.vertices[4]),new Line(this.vertices[1],this.vertices[5]),
-            new Line(this.vertices[2],this.vertices[6]),new Line(this.vertices[3],this.vertices[7])];
-
-        this.walls = [new wall(this.vertices[0],this.vertices[1],this.vertices[2],this.vertices[3]),
-            new wall(this.vertices[0],this.vertices[1],this.vertices[5],this.vertices[4]),
-            new wall(this.vertices[1],this.vertices[2],this.vertices[6],this.vertices[5]),
-            new wall(this.vertices[2],this.vertices[3],this.vertices[7],this.vertices[6]),
-            new wall(this.vertices[3],this.vertices[0],this.vertices[4],this.vertices[7]),
-            new wall(this.vertices[4],this.vertices[5],this.vertices[6],this.vertices[7])]
-    }
-
-    function wall(p1, p2, p3, p4) {
-        this.points = new Array;
-        this.points[0] = p1;
-        this.points[1] = p2;
-        this.points[3] = p3;
-        this.points[4] = p4;
-    }
-
+    let objectPool = [];
+    let initsize = 10;
 
     // let cubeZero = new Cube(-10, 10,-10,20,-20,20,1);
     // cubeZero.lines.forEach(line => objectPool.push(line));
 
-    let cubes = [ new Cube(0,0,0,1,-1,1,100),
-        new Cube(200,0,0,1,-1,1,100),
-        new Cube(0,0,200,1,-2,1,100),
-        new Cube(200,0,200,1,-2,1,100)]
+    let cubes = [new Cube(0, 0, 0, 1, -1, 1, 100),
+        new Cube(200, 0, 0, 1, -1, 1, 100),
+        new Cube(0, 0, 200, 1, -2, 1, 100),
+        new Cube(200, 0, 200, 1, -2, 1, 100)];
 
     cubes.forEach(cube => cube.lines.forEach(line => objectPool.push(line)));
 
-    var renderPool = [];
+    let renderPool = [];
 
-    var cam = new Camera(150, -200, -200, -0.5, 0, 0, 1.5);
+    let cam = new Camera(150, -200, -200, -0.5, 0, 0, 1.5);
 
     this.initialize = function () {
 
@@ -259,31 +214,27 @@ var screen3D = new function () {
             cam.zoom -= 0.05;
         }
 
-        let ptn, ptn2;
-        for (var i = 0, len = objectPool.length; i < len; i++) {
-            var object = objectPool[i];
-            if (typeof object === 'undefined') {
-                continue;
-            }
-
-            ptn = object.get2DCoords(cam ,0);
-            ptn2 = object.get2DCoords(cam ,1);
+        objectPool.forEach((line) => {
+            let ptn = line.points[0].get2DCoords(cam);
+            let ptn2 = line.points[1].get2DCoords(cam);
             if ((ptn.x < -screen.width) || (ptn.y < -screen.height) || (ptn.x > screen.width * 2) || (ptn.y > screen.height * 2) || (ptn.distance < 0)
-            || (ptn2.x < -screen.width) || (ptn2.y < -screen.height) || (ptn2.x > screen.width * 2) || (ptn2.y > screen.height * 2) || (ptn2.distance < 0)) {
+                || (ptn2.x < -screen.width) || (ptn2.y < -screen.height) || (ptn2.x > screen.width * 2) || (ptn2.y > screen.height * 2) || (ptn2.distance < 0)) {
             } else {
-                renderPool.push(object);
+                renderPool.push(line);
             }
-        }
+        });
+
         renderPool.sort(function (a, b) {
             return b.tempIndex - a.tempIndex;
         });
 
         contxt.fillStyle = "rgba(0,0,0,1)";
         contxt.clearRect(0, 0, screen.width, screen.height);
-        for (var i = 0, len = renderPool.length; i < len; i++) {
-            var object = renderPool[i];
-            object.render(cam, contxt, 1);
-        }
+
+        renderPool.forEach(object => {
+            object.render(cam,contxt,1);
+        });
+
         renderPool = [];
 
         //console.log(cam.position.x + ","+ cam.position.y + "," + cam.position.z + "," + cam.rotation.x + "," + cam.rotation.y + "," + + cam.rotation.z + "," + cam.zoom);
@@ -303,22 +254,23 @@ function Camera(x, y, z, xo, yo, zo, zoom) {
     };
     this.zoom = zoom;
 }
-Camera.prototype.moveFB = function (v) {
-    var cosy = Math.cos(-this.rotation.y);
-    var siny = Math.sin(-this.rotation.y);
 
-    var nx = (-siny * (v));
-    var nz = (cosy * (v));
+Camera.prototype.moveFB = function (v) {
+    let cosy = Math.cos(-this.rotation.y);
+    let siny = Math.sin(-this.rotation.y);
+
+    let nx = (-siny * (v));
+    let nz = (cosy * (v));
 
     this.position.x += nx;
     this.position.z += nz;
 };
 Camera.prototype.moveLR = function (v) {
-    var cosy = Math.cos(-this.rotation.y);
-    var siny = Math.sin(-this.rotation.y);
+    let cosy = Math.cos(-this.rotation.y);
+    let siny = Math.sin(-this.rotation.y);
 
-    var nx = (cosy * (v));
-    var nz = (siny * (v));
+    let nx = (cosy * (v));
+    let nz = (siny * (v));
 
     this.position.x += nx;
     this.position.z += nz;
@@ -326,6 +278,61 @@ Camera.prototype.moveLR = function (v) {
 
 Camera.prototype.moveY = function (y) {
     this.position.y += y;
+};
+
+
+function Cube(x, y, z, x_size, y_size, z_size, multip) {
+    this.position = {
+        x: x,
+        y: y,
+        z: z
+    };
+    this.size = {
+        x: x_size * multip,
+        y: y_size * multip,
+        z: z_size * multip,
+    };
+    this.vertices = [new Point(x, y, z), new Point(x + this.size.x, y, z), new Point(x + this.size.x, y, z + this.size.z), new Point(x, y, z + this.size.z),
+        new Point(x, y + this.size.y, z), new Point(x + this.size.x, y + this.size.y, z), new Point(x + this.size.x, y + this.size.y, z + this.size.z), new Point(x, y + this.size.y, z + this.size.z)];
+
+    this.lines = [new Line(this.vertices[0], this.vertices[1]), new Line(this.vertices[1], this.vertices[2]),
+        new Line(this.vertices[2], this.vertices[3]), new Line(this.vertices[3], this.vertices[0]),
+
+        new Line(this.vertices[4], this.vertices[5]), new Line(this.vertices[5], this.vertices[6]),
+        new Line(this.vertices[6], this.vertices[7]), new Line(this.vertices[7], this.vertices[4]),
+
+        new Line(this.vertices[0], this.vertices[4]), new Line(this.vertices[1], this.vertices[5]),
+        new Line(this.vertices[2], this.vertices[6]), new Line(this.vertices[3], this.vertices[7])];
+
+    this.walls = [new Wall(this.vertices[0], this.vertices[1], this.vertices[2], this.vertices[3]),
+        new Wall(this.vertices[0], this.vertices[1], this.vertices[5], this.vertices[4]),
+        new Wall(this.vertices[1], this.vertices[2], this.vertices[6], this.vertices[5]),
+        new Wall(this.vertices[2], this.vertices[3], this.vertices[7], this.vertices[6]),
+        new Wall(this.vertices[3], this.vertices[0], this.vertices[4], this.vertices[7]),
+        new Wall(this.vertices[4], this.vertices[5], this.vertices[6], this.vertices[7])]
+}
+
+function Wall(p1, p2, p3, p4) {
+    this.points = [p1, p2, p3, p4];
+}
+
+Wall.prototype.render = function (cam) {
+    console.log("0");
+};
+
+function Line(p1, p2) {
+    this.points = [p1, p2];
+    //this.tempIndex = 0;
+}
+
+Line.prototype.render = function (cam, cont) {
+    let screenCoords = this.points[0].get2DCoords(cam);
+    let screenCoords2 = this.points[1].get2DCoords(cam);
+    cont.beginPath();
+    cont.moveTo(screenCoords.x, screenCoords.y);
+    cont.lineTo(screenCoords2.x, screenCoords2.y);
+    cont.strokeStyle = '#0000FF';
+    cont.stroke();
 };
 
 function Point(x, y, z) {
@@ -339,20 +346,20 @@ function Point(x, y, z) {
 
 Point.prototype.get2DCoords = function (c) {
 
-    var dx = this.position.x - c.position.x;
-    var dy = this.position.y - c.position.y;
-    var dz = this.position.z - c.position.z;
+    let dx = this.position.x - c.position.x;
+    let dy = this.position.y - c.position.y;
+    let dz = this.position.z - c.position.z;
 
-    var cosx = Math.cos(c.rotation.x);
-    var cosy = Math.cos(c.rotation.y);
-    var cosz = Math.cos(c.rotation.z);
-    var sinx = Math.sin(c.rotation.x);
-    var siny = Math.sin(c.rotation.y);
-    var sinz = Math.sin(c.rotation.z);
+    let cosx = Math.cos(c.rotation.x);
+    let cosy = Math.cos(c.rotation.y);
+    let cosz = Math.cos(c.rotation.z);
+    let sinx = Math.sin(c.rotation.x);
+    let siny = Math.sin(c.rotation.y);
+    let sinz = Math.sin(c.rotation.z);
 
-    var nx = (cosy * (sinz * (dy) + cosz * (dx)) - siny * (dz));
-    var ny = (sinx * (cosy * (dz) + siny * (sinz * (dy) + cosz * (dx))) + cosx * (cosz * (dy) - sinz * (dx)));
-    var nz = (cosx * (cosy * (dz) + siny * (sinz * (dy) + cosz * (dx))) - sinx * (cosz * (dy) - sinz * (dx)));
+    let nx = (cosy * (sinz * (dy) + cosz * (dx)) - siny * (dz));
+    let ny = (sinx * (cosy * (dz) + siny * (sinz * (dy) + cosz * (dx))) + cosx * (cosz * (dy) - sinz * (dx)));
+    let nz = (cosx * (cosy * (dz) + siny * (sinz * (dy) + cosz * (dx))) - sinx * (cosz * (dy) - sinz * (dx)));
 
 
     //this.tempIndex = nz;
@@ -361,28 +368,6 @@ Point.prototype.get2DCoords = function (c) {
         y: (((ny) * (c.zoom / nz)) * (screen.height / 2)) + (screen.height / 2),
         distance: nz
     };
-};
-
-function Line(p1, p2) {
-    this.points = new Array;
-    this.points[0] = p1;
-    this.points[1] = p2;
-    //this.tempIndex = 0;
-};
-
-Line.prototype.get2DCoords = function (cam, i) {
-    var screenCoords = this.points[i].get2DCoords(cam);
-    //this.tempIndex = this.points[i].tempIndex;
-    return (screenCoords);
-};
-Line.prototype.render = function (cam, cont) {
-    var screenCoords = this.points[0].get2DCoords(cam,0);
-    var screenCoords2 = this.points[1].get2DCoords(cam,0);
-    cont.beginPath();
-    cont.moveTo(screenCoords.x, screenCoords.y);
-    cont.lineTo(screenCoords2.x, screenCoords2.y);
-    cont.strokeStyle = '#0000FF';
-    cont.stroke();
 };
 
 screen3D.initialize();
